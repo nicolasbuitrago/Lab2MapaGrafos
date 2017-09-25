c2eff1be42a5cafa4562aec13b2ed43e53cf2ea6
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2017-09-25 14:46:05 -0500

    First commit

---
+++ src/codigo/Codigo.java
@@ -0,0 +1,16 @@
+package codigo;
+
+/**
+ *
+ * @author nicolasbuitrago
+ */
+public class Codigo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
