5de592d279b3fe7243ff8482934d524b925dae3b
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2017-10-04 15:21:42 -0500

    Problema al mostrar la hora de llegada solucionado

--- src/codigo/model/Tiempo.java
+++ src/codigo/model/Tiempo.java
@@ -39,7 +39,7 @@ public class Tiempo extends Thread{
     
     public void start(double minutos){
         this.time = LocalTime.now();
-        this.time.plusMinutes((long)minutos); System.out.println("Tiempo = "+minutos);
+        this.time = this.time.plusMinutes((long)minutos); System.out.println("Tiempo = "+minutos);
         label.setText(fromHoraMilitar());
         if(isAlive()){
             this.stop();
